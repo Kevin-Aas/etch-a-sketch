17768f999014553c4805537678cac22ce73be961
Kevin Aslesen <kevinalexander@live.no>
2023-12-08 19:49:09 +0100

    Make div change color when hovered over

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 for (let i=0; i<16*16; i++) {
+    // Get container and make new div
     let container = document.querySelector(".container");
-    let new_div = document.createElement('div');
-    new_div.style.width = '20px';
-    new_div.style.height = '20px';
-    new_div.style.backgroundColor = "black";
-    container.appendChild(new_div);
+    let div = document.createElement('div');
+    // Style the new div:
+    div.style.width = '20px';
+    div.style.height = '20px';
+    div.style.backgroundColor = "black";
+    // Add event listener to new div:
+    div.addEventListener('mouseover', function () {
+        div.style.backgroundColor = "red"; // div changes color when mouseover
+    });
+    container.appendChild(div);
 }
