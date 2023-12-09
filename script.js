b72a6c2609494f926bed23b5a8dcaa8fd9d3f06c
Kevin Aslesen <kevinalexander@live.no>
2023-12-09 02:12:28 +0100

    Make prompt button for size preference

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-for (let i=0; i<16*16; i++) {
-    // Get container and make new div
-    let container = document.querySelector(".container");
-    let div = document.createElement('div');
-    // Style the new div:
-    div.style.width = '20px';
-    div.style.height = '20px';
-    div.style.backgroundColor = "black";
-    // Add event listener to new div:
-    div.addEventListener('mouseover', function () {
-        div.style.backgroundColor = "red"; // div changes color when mouseover
-    });
-    container.appendChild(div);
+// Set up intial grid
+let size = 16;
+make_grid(size);
+
+// Give a prompt when clicking button
+let btn = document.querySelector(".prompt_btn");
+btn.addEventListener('click', function () {
+    let answer = prompt("How man squares per side?");
+    size = parseInt(answer);
+    make_grid(size);
+});
+
+function make_grid (size) {
+    // Get body and old container reference:
+    let body = document.querySelector("body");
+    let old_container = document.querySelector('.container');
+    // Set up new container:
+    let container = document.createElement('div');
+    container.setAttribute('class', 'container');
+
+    // Make size amount of div tiles for the container:
+    for (let i=0; i<size*size; i++) {
+        let div = document.createElement('div');
+        // Style the new div:
+        let div_size = old_container.clientWidth / size;
+        div.style.width = `${div_size}px`;
+        div.style.height = `${div_size}px`;
+        div.style.backgroundColor = "black";
+        // Add event listener to new div:
+        div.addEventListener('mouseover', function () {
+            div.style.backgroundColor = "red"; // div changes color when mouseover
+        });
+        container.appendChild(div);
+    }
+
+    // Replace the old container with the new:
+    body.replaceChild(container, old_container);
 }
